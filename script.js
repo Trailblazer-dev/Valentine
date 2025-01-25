da34ff5d701dfd4dd2c784c9d2b990aef9eecc08
Trailblazer-dev <richvictor830@gmail.com>
2025-01-25 22:39:25 +0300

    Done building

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const noBtn = document.querySelector('.no-btn');
 
 yesBtn.addEventListener('click', ()=>{
     question.innerHTML = "YAaaaaaaayyyy!!"
-    gif.src = "images/jump.gif";
+    gif.src = "./bunnies-jump-on-top-of-you.gif";
 });
 
 noBtn.addEventListener('mouseover', ()=>{
